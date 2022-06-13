ed70678705250f56dd03113a44af493c28638002
Néo ALMEIDA <neo.almeida@bettercalldave.io>
2022-06-13 10:22:38 +0200

    Class to choose the current fractal + handle default things (like if it's mandelbrot, use the colorpalet X and X iterations)

---
+++ src/FractalHandler.cpp
@@ -0,0 +1,5 @@
+#include "FractalHandler.hpp"
+
+FractalHandler::FractalHandler() {
+    this->current = Fractal(Fractal::Mandelbrot);
+};
---
+++ src/FractalHandler.hpp
@@ -0,0 +1,15 @@
+#ifndef FRACTALHANDLER_HPP
+#define FRACTALHANDLER_HPP
+
+#include "Fractal.hpp"
+
+class FractalHandler {
+
+public:
+    FractalHandler();
+    Fractal current;
+
+};
+
+
+#endif
