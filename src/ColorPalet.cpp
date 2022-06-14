d5c7a4abce4fa7fab4a1cf566b4091592f4f97e0
Néo ALMEIDA <neo.almeida@bettercalldave.io>
2022-06-14 14:37:11 +0200

    Fixing error about multiple definitions / Add methods

--- src/ColorPalet.cpp
+++ src/ColorPalet.cpp
@@ -1,15 +1,39 @@
 #include "ColorPalet.hpp"
 
+sf::Glsl::Vec4* palletToArray(std::vector<sf::Glsl::Vec4> pallet) {
+    sf::Glsl::Vec4* array = new sf::Glsl::Vec4[pallet.size()];
+    for (size_t i = 0; i < pallet.size(); i++) {
+        array[i] = pallet[i];
+    }
+    return array;
+}
+
 ColorPalet::ColorPalet() {
     //std::vector<std::pair<std::string, std::vector<sf::Glsl::Vec4>>> pallet_pairs = {};
-    std::map<std::string, std::vector<sf::Glsl::Vec4>> pallet_pairs = {};
+    this->pallet_pairs = {};
 }
 
 void ColorPalet::registerPallet(std::string label, std::vector<sf::Glsl::Vec4> pallet) {
     this->pallet_pairs.insert(std::pair<std::string, std::vector<sf::Glsl::Vec4>>(label, pallet));
 }
 
-std::vector<sf::Glsl::Vec4> ColorPalet::getPallet(int index) {
-    return this->pallet_pairs[index].second;
+std::vector<sf::Glsl::Vec4> ColorPalet::getPallet(std::string label) {
+    //return this->pallet_pairs[index].second;
+    return this->pallet_pairs.at(label);
+}
+
+std::vector<sf::Glsl::Vec4> ColorPalet::getPallet(size_t index) {
+    if (this->pallet_pairs.size() > index) {
+        size_t i = 0;
+        for (auto const& [key, val] : this->pallet_pairs) {
+            if (i == index) {
+                return val;
+            }
+            i++;
+        }
+    } else {
+        std::cout << "Error: ColorPalet::getPallet(int index) - index out of range" << std::endl;
+        return std::vector<sf::Glsl::Vec4>();
+    }
 }
 
--- src/Fractal.cpp
+++ src/Fractal.cpp
@@ -6,5 +6,21 @@ Fractal::Fractal(Type type) {
     this->iterations = 100;
     this->scale = 1.0f;
     this->center = sf::Glsl::Vec2(0.0f, 0.0f);
-    this->pallet = rgb_pallete;
+    this->pallet = GLOBAL_PALLET.getPallet((size_t)0);
+}
+
+Fractal::Fractal(int type) {
+    this->type = (Type)type;
+    this->iterations = 100;
+    this->scale = 1.0f;
+    this->center = sf::Glsl::Vec2(0.0f, 0.0f);
+    this->pallet = GLOBAL_PALLET.getPallet((size_t)0);
+}
+
+Fractal::Fractal() {
+    this->type = Type::Mandelbrot;
+    this->iterations = 100;
+    this->scale = 1.0f;
+    this->center = sf::Glsl::Vec2(0.0f, 0.0f);
+    this->pallet = GLOBAL_PALLET.getPallet((size_t)0);
 }
