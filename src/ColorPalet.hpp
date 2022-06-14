0f2e38a484148fa0b2c942782eef423a4b780f27
Néo ALMEIDA <neo.almeida@bettercalldave.io>
2022-06-14 14:38:26 +0200

    fix error about multiple definitions

--- src/ColorPalet.hpp
+++ src/ColorPalet.hpp
@@ -4,21 +4,18 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-sf::Glsl::Vec4* palletToArray(std::vector<sf::Glsl::Vec4> pallet) {
-    sf::Glsl::Vec4* array = new sf::Glsl::Vec4[pallet.size()];
-    for (size_t i = 0; i < pallet.size(); i++) {
-        array[i] = pallet[i];
-    }
-    return array;
-}
+sf::Glsl::Vec4* palletToArray(std::vector<sf::Glsl::Vec4> pallet);
 
 class ColorPalet {
     public:
         ColorPalet();
         
-        std::vector<std::pair<std::string, std::vector<std::vector<sf::Glsl::Vec4>>>> pallet_pairs;
+        //std::vector<std::pair<std::string, std::vector<std::vector<sf::Glsl::Vec4>>>> pallet_pairs;
+        std::map<std::string, std::vector<sf::Glsl::Vec4>> pallet_pairs;
 
-        std::vector<sf::Glsl::Vec4> getPallet(int index);
+        std::vector<sf::Glsl::Vec4> getPallet(std::string label);
+        std::vector<sf::Glsl::Vec4> getPallet(size_t index);
+
         void registerPallet(std::string label, std::vector<sf::Glsl::Vec4> pallet);
         //std::vector<std::vector<sf::Glsl::Vec4>> getPalletByLabel(std::string label);
 };
